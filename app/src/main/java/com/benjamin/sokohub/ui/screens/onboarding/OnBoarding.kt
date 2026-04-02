df8401ae62fac8a5339aba28fb5ae821fef8d3c5
perizdonah3 <perizdonah3@gmail.com>
2026-04-02 13:42:23 +0300

    new home screen

--- app/src/main/java/com/benjamin/sokohub/ui/screens/onboarding/OnBoarding.kt
+++ app/src/main/java/com/benjamin/sokohub/ui/screens/onboarding/OnBoarding.kt
@@ -1,27 +1,99 @@
 package com.benjamin.sokohub.ui.screens.onboarding
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.benjamin.sokohub.R
-@Composable
-fun OnBoardingScreen(){
+import com.benjamin.sokohub.ui.screens.home.HomeScreen
+import com.benjamin.sokohub.ui.theme.OLdNavy
 
+@Composable
+fun OnboardingScreen(){
     Column(
-        modifier = Modifier.fillMaxSize()
-    ) {
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ){
 
         Image(
             painter = painterResource(R.drawable.product),
-            contentDescription = "product",
+            contentDescription = "img",
             modifier = Modifier.size(300.dp)
 
+
+
+
+
+
+
         )
+
+        Spacer(modifier = Modifier.height(20.dp) )
+
+        Text(
+            text = "Welcome to Sokohub!!",
+            fontSize = 30.sp,
+            fontWeight = FontWeight.Bold,
+            fontFamily = FontFamily.SansSerif,
+            color =OLdNavy
+        )
+        Text(
+            text = "Shop Smarter,",
+            fontSize = 20.sp,
+
+
+
+
+            )
+        Text(
+            text = "Everywhere you go,",
+            fontSize = 20.sp,
+
+
+            )
+
+        Text(
+            text = "Welcome to our store—where quality meets convenience. We’re committed to providing a smooth and enjoyable shopping experience with carefully selected products, secure payments, and fast, reliable delivery you can trust. Our team is always ready to assist you and ensure your satisfaction every step of the way. Thank you for choosing us as your shopping destination, and we look forward to serving you.\n,",
+            fontSize = 16.sp,
+            textAlign = TextAlign.Justify
+        )
+        Button(
+            onClick = {},
+            colors = ButtonDefaults.buttonColors(OLdNavy),
+            shape = RoundedCornerShape(10.dp),
+            modifier = Modifier.width(350.dp)
+
+        ) {
+            Text(text = "Get Started")
+        }
+
+
+
+
+
+
+
+
 
 
 
@@ -34,44 +106,62 @@ fun OnBoardingScreen(){
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
+@Preview(showBackground = true)
+@Composable
+fun OnboardingScreenPreview(){
+    OnboardingScreen()
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 @Preview(showBackground = true)
 @Composable
 fun OnBoardingPreview(){
-    OnBoardingScreen()
+    OnboardingScreen()
 
 
 
